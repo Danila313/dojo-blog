3ee183885b568628650cee0b8460c2ec66bf04a0
Danila313 <105373392+Danila313@users.noreply.github.com>
2022-05-23 09:51:04 +0200

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                {/* Uso Link to per evitare di fare una richiesta al server per le nuove pagine, in questo modo viene intercettato dal react router */}
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
